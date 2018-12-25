1f7ec9dee98b82ae8771b028f3e5fc32e7b1652f
husseyexplores <husseyexplores@gmail.com>
2018-12-25 20:45:43 +0500

    Add storepicker functionality

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,30 @@
 import React, { Fragment, Component } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  storeInput = React.createRef();
+
+  goToStore = e => {
+    e.preventDefault();
+    // get the input text
+    const storeName = this.storeInput.current.value;
+
+    // redirect to that store url
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
       <Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name" />
+          <input
+            type="text"
+            ref={this.storeInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store →</button>
         </form>
       </Fragment>
